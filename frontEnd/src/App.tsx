452bb11865729e38c03db79d53024bfde5b35a83
Jean de Castro Aleixo <jean.castroaleixo@gmail.com>
2025-08-27 23:50:28 -0300

    feat/header home

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,10 +1,18 @@
-import Header from "./components/headerHome"
+import HeaderHome from "./components/Home/headerHome"
 
 function App() {
   return (
     <>
       <div>
-        <Header/>
+        <HeaderHome/>
+        <div className="flex items-center mx-auto max-w-screen-xl">
+          <section className="flex">
+            <div className="flex">
+              <div className="flex justify-start"><span>Apoiamos empreendedores na <br /> realização de seus sonhos,<br /> simplificando a gestão de micro e<br /> pequenos negócios.</span></div>
+              <div className="flex justify-end"><span>imagem aqui</span></div>
+            </div>
+          </section>
+        </div>
       </div>
     </>
   )
