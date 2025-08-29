3ca39f1a9f091b5374025e7cc62a3cb882cca64b
Jean de Castro Aleixo <jean.castroaleixo@gmail.com>
2025-08-29 08:06:47 -0300

    feat/hero section

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,17 +1,25 @@
 import HeaderHome from "./components/Home/headerHome"
+import hearoFigure from "./assets/heroFigure.png"
 
 function App() {
   return (
     <>
       <div>
         <HeaderHome/>
-        <div className="flex items-center mx-auto max-w-screen-xl">
-          <section className="flex">
-            <div className="flex">
-              <div className="flex justify-start"><span>Apoiamos empreendedores na <br /> realização de seus sonhos,<br /> simplificando a gestão de micro e<br /> pequenos negócios.</span></div>
-              <div className="flex justify-end"><span>imagem aqui</span></div>
+        <div className="flex items-center w-auto">
+          <section className="w-screen py-32 bg-amber-100">
+            <div className="mx-auto max-w-screen-xl w-full px-4">
+              <div className="flex w-full justify-between items-center my-10">
+                <div className="w-1/2">
+                  <span className="text-3xl">Apoiamos empreendedores na <br /> realização de seus sonhos,<br /> simplificando a gestão de micro e<br /> pequenos negócios.</span>
+                </div>
+                <div className="w-1/2 flex justify-end">
+                  <img className="max-w-full w-80 lg:w-[480px] h-auto" src={hearoFigure} alt="" />
+                </div>
+              </div>
             </div>
           </section>
+          <section className=""></section>
         </div>
       </div>
     </>
--- frontEnd/src/components/Home/headerHome.tsx
+++ frontEnd/src/components/Home/headerHome.tsx
@@ -3,13 +3,13 @@ import HeaderHomeLogo from "../../assets/corestock.png"
 export default function HeaderHome() {
 
   return (
-    <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 dark:bg-gray-800">
+    <header className="bg-white shadow-lg">
+      <nav className="bg-white border-b border-gray-200 px-4 lg:px-6">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <img src={HeaderHomeLogo} className="mr-3 h-20 w-40" alt="CoreStock Logo" />
           <div className="flex items-center lg:order-2">
-            <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</a>
-            <a href="#" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Contrate</a>
+            <a href="#" className="text-yellow-600 hover:text-yellow-700 focus:ring-4 focus:ring-yellow-100 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Login</a>
+            <a href="#" className="bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-200 text-white font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Contrate</a>
             <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -19,10 +19,10 @@ export default function HeaderHome() {
           <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
-                <a href="#" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Casos de Uso</a>
+                <a href="#" className="block py-2 pr-4 pl-3  hover:bg-gray-50 lg:hover:bg-transparent  lg:p-0 text-yellow-600 hover:text-yellow-700">Casos de Uso</a>
               </li>
               <li>
-                <a href="#" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Integrações</a>
+                <a href="#" className="block py-2 pr-4 pl-3  hover:bg-gray-50 lg:hover:bg-transparent lg:p-0 text-yellow-600 hover:text-yellow-700">Integrações</a>
               </li>
             </ul>
           </div>
